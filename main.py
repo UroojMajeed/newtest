026026442a22e680b325e0425b8a0ba2f1fbc4a2
Urooj Majeed <uroojmajeed32@gmail.com>
2022-02-19 23:12:16 +0500

    jj

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def hello():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if lr:
         try:
             json_ = request.json
             print(json_)
@@ -27,17 +26,15 @@ def predict():
             return jsonify({'prediction': str(prediction)})
 
         except:
-
             return jsonify({'trace': traceback.format_exc()})
-    else:
-        print ('Train the model first')
-        return ('No model here to use')
+
 
 if __name__ == '__main__':
     try:
         port = int(sys.argv[1]) # This is for a command-line input
     except:
-        port = 12345 # If you don't provide any port the port will be set to 12345
+        port = int(os.environ.get('PORT', 33507))
+ # If you don't provide any port the port will be set to 12345
 
     lr = joblib.load("model.pkl") # Load "model.pkl"
     print ('Model loaded')
